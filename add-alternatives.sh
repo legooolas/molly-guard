6dbe659678ff330986905fb60e0e2a87b88eb208
David Gardner <david.gardner@bristol.ac.uk>
2015-09-17 09:52:41 +0100

    Fix typo in alternatives command, plus copy existing binaries to molly-guard directory before clobbering with alternatives links.

--- add-alternatives.sh
+++ add-alternatives.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# Before we clobber anything, copy the files (which exist) to /lib/molly-guard:
+for file in /sbin/poweroff /sbin/reboot /sbin/shutdown /sbin/coldreboot /sbin/pm-hibernate /sbin/pm-suspend /sbin/pm-suspend/hybrid ; do
+  cp -av "${file}" /lib/molly-guard/
+done
+
+# TODO : What to do with the /usr/bin versions?  Currently we just replace them with molly-guard versions.
+
+
 alternatives --install /sbin/halt halt              /lib/molly-guard/molly-guard 999 \
-  --slave /sbin/powreoff          poweroff          /lib/molly-guard/molly-guard \
+  --slave /sbin/poweroff          poweroff          /lib/molly-guard/molly-guard \
   --slave /sbin/reboot            reboot            /lib/molly-guard/molly-guard \
   --slave /sbin/shutdown          shutdown          /lib/molly-guard/molly-guard \
   --slave /sbin/coldreboot        coldreboot        /lib/molly-guard/molly-guard \
